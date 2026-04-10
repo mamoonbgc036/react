360fd3d00f1d86a357b282c9c97b3b1853f3c1e3
mamoonbgc036 <mamoon036@gmail.com>
2026-04-10 16:50:26 +0600

    usecontext done

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,9 +1,16 @@
 import Navbar from './Navbar.tsx'
+import { AuthContext } from './context/AuthContext.tsx'
+import { useContext } from 'react'
 
 export default function Layout() {
+    const auth = useContext(AuthContext)
+    if (!auth) return null;
+    const { user, login, logout } = auth
     return (
         <>
             <h2>My App</h2>
+            {user}
+            {user ? <button onClick={logout}>Logout</button> : <button onClick={login}>Login</button>}
             <Navbar />
         </>
     )
